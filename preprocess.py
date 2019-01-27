76228a3aa8fb15e312e48775ea4d5a1077863dac
AdirthaBorgohain <adirthaborgohain@gmail.com>
2019-01-27 13:56:07 +0530

    Imported the dataset variables

--- preprocess.py
+++ preprocess.py
@@ -2,35 +2,29 @@ import numpy as np
 import pandas as pd
 
 
-def get_ratings(csv_rating):
-    ratings = pd.read_csv(csv_rating, sep='\t', encoding='latin-1',
-                          usecols=['user_id', 'movie_id', 'user_emb_id', 'movie_emb_id', 'rating'])
-    max_userid = ratings['user_id'].drop_duplicates().max()
-    max_movieid = ratings['movie_id'].drop_duplicates().max()
+def get_ratings(csv):
+    ratings = pd.read_csv(csv, sep=',', encoding='latin-1',
+                          usecols=['userId', 'movieId', 'rating'])
+    max_userid = ratings['userId'].drop_duplicates().max()
+    max_movieid = ratings['movieId'].drop_duplicates().max()
     return ratings, max_userid, max_movieid
 
 
-def get_users(csv_users):
-    users = pd.read_csv(csv_users, sep='\t', encoding='latin-1',
-                        usecols=['user_id', 'gender', 'zipcode', 'age_desc', 'occ_desc'])
-    return users
-
-
-def get_movies(csv_movies):
-    movies = pd.read_csv(csv_movies, sep='\t', encoding='latin-1',
-                         usecols=['movie_id', 'title', 'genres'])
+def get_movies(csv):
+    movies = pd.read_csv(csv, sep=',', encoding='latin-1',
+                         usecols=['movieId', 'title', 'genres'])
     return movies
 
 
-def create_train_set():
-    ratings = get_ratings('ratings.csv')
-    shuffled_ratings = ratings.sample(frac=1., random_state=RNG_SEED)
+def create_train_set(csv):
+    ratings, _, _ = get_ratings(csv)
+    shuffled_ratings = ratings.sample(frac=1., random_state=np.random.RandomState)
     return shuffled_ratings
 
 
-def shuffle():
-    shuffled_ratings = create_train_set()
-    Users = shuffled_ratings['user_emb_id'].values
-    Movies = shuffled_ratings['movie_emb_id'].values
+def shuffleData(csv):
+    shuffled_ratings = create_train_set(csv)
+    Users = shuffled_ratings['userId'].values
+    Movies = shuffled_ratings['movieId'].values
     Ratings = shuffled_ratings['rating'].values
     return Users, Movies, Ratings
